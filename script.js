8e9b7fe711b3ff2a183fd1a82c7bfb9a20397f3f
ogbueze <ogbuezeokenyi@gmail.com>
2025-12-05 19:37:35 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('registration-form')
+
+const feedbackDiv = document.getElementById('form-feedback')
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    const username = document.getElementById('username').value.trim()
+    const email = document.getElementById('email').value.trim()
+    const password = document.getElementById('password').value.trim()
+
+    let isValid = true
+    const message = []
+
+    if(username.length < 3){
+        isValid = false
+        message.push('User name must be longer than 3 characters.')
+    }
+
+    if(password.length < 8){
+        isValid = false
+        message.push('password must be at least 8 characters long')
+    }
+    feedbackDiv.style.display = 'block'
+    if(isValid){
+        feedbackDiv.textContent = 'Registration successful!'
+        feedbackDiv.style.color = '#28a745'
+        form.submit();
+       
+        
+        
+    }else{
+        const result = message.join('<br>')
+        feedbackDiv.innerHTML = result
+        feedbackDiv.style.color = '#dc3545'
+        console.log(result)
+    }
+
+    
+})
